6e8b2994cc332d3f03ac53bca4244236a68b473d
Weihan Li <weihanli101@gmail.com>
2018-08-07 11:33:16 -0400

    added camera controller + moved camera to top down view

---
+++ Assets/scripts/CameraController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+    public GameObject playerGameObj;
+
+    private Vector3 cameraOffest;
+
+
+	// Use this for initialization
+	void Start () {
+        cameraOffest = transform.position - playerGameObj.transform.position;	
+	}
+
+    //LateUpdate called after update (so the player moves then the camera follows)
+    private void LateUpdate() {
+        transform.position = playerGameObj.transform.position + cameraOffest;
+    }
+}
