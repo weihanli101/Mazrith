fd5c47efa9219dec2a4e533d16b487e740f59289
bheffernan <bheffernan@blackberry.com>
2018-08-08 15:32:30 -0400

    added basic enemy controller to follow player

---
+++ Assets/scripts/enemyController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyController : MonoBehaviour
+{
+    int MoveSpeed = 5;
+
+    public Transform Player;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(Player);
+        transform.position += transform.forward * MoveSpeed * Time.deltaTime;
+    }
+}
