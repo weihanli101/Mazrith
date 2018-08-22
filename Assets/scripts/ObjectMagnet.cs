0040e0d30b51c9d2e1ede2f60f3772c429971bee
Weihan Li <weihanli101@gmail.com>
2018-08-22 01:29:17 -0400

    added in pulling script for attracting light orbs

---
+++ Assets/scripts/ObjectMagnet.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class ObjectMagnet : MonoBehaviour {
+    public float pullRadius;
+    public float pullForce;
+
+	void FixedUpdate () {
+        foreach (Collider collider in Physics.OverlapSphere(transform.position, pullRadius)){
+            
+            if (collider.CompareTag("lightorb")){
+
+                Debug.Log(collider);
+                // calculate direction from target to me
+                Vector3 forceDirection = transform.position - collider.transform.position;
+
+                // apply force on target towards me
+                collider.GetComponent<Rigidbody>().AddForce(forceDirection.normalized * pullForce * Time.fixedDeltaTime);
+            }
+        }
+    }
+}
