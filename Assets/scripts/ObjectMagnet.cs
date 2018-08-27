1cbe05e29e891431cdb7f380fd3e433f049ec25d
Weihan Li <weihanli101@gmail.com>
2018-08-27 02:21:41 -0400

    added range slider to stopping freq option in enemyController + added ablity to chase when player gets too close

--- Assets/scripts/ObjectMagnet.cs
+++ Assets/scripts/ObjectMagnet.cs
@@ -8,11 +8,8 @@ public class ObjectMagnet : MonoBehaviour {
         foreach (Collider collider in Physics.OverlapSphere(transform.position, pullRadius)){
             
             if (collider.CompareTag("lightorb")){
-
-                Debug.Log(collider);
                 // calculate direction from target to me
                 Vector3 forceDirection = transform.position - collider.transform.position;
-
                 // apply force on target towards me
                 collider.GetComponent<Rigidbody>().AddForce(forceDirection.normalized * pullForce * Time.fixedDeltaTime);
             }
--- Assets/scripts/enemyController.cs
+++ Assets/scripts/enemyController.cs
@@ -1,37 +1,51 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class EnemyController : MonoBehaviour {
     public int moveSpeed;
     public float rotationDamping;
+    [Range(0, 100)]
     public float pausePercentage;
+    public float alertRadius;
+    public float alertedMovementSpeed;
+
     private float timeToChange;
     private float desiredRotation;
     private float randomSeed;
+    private bool isAlerted;
 
-    // Use this for initialization
     void Start() {
         ChangeDirection(false);
         randomSeed = Random.Range(0, 100);
     }
 
-    // Update is called once per frame
     void Update() {
-        timeToChange -= Time.deltaTime;
-        
-        if(timeToChange <= 0) {
-            ChangeDirection(false);
+        foreach(Collider collider in Physics.OverlapSphere(transform.position, alertRadius)) {
+            if(collider.CompareTag("Player")) {
+                isAlerted = true;
+                Vector3 forceDirection = collider.transform.position - transform.position;
+                transform.position += forceDirection.normalized * alertedMovementSpeed * Time.deltaTime;
+            } else {
+                isAlerted = false;
+            }
         }
-        var desiredRotationQuat = Quaternion.Euler(transform.eulerAngles.x, desiredRotation, transform.eulerAngles.z);
-        transform.rotation = Quaternion.Lerp(transform.rotation, desiredRotationQuat, Time.deltaTime * rotationDamping);
-        if(randomSeed <= pausePercentage) {
-            transform.position += transform.forward * Time.deltaTime * moveSpeed;
+        // roaming mode when AI is not alerted
+        if(!isAlerted) {
+            timeToChange -= Time.deltaTime;
+
+            if (timeToChange <= 0) {
+                ChangeDirection(false);
+            }
+            var desiredRotationQuat = Quaternion.Euler(transform.eulerAngles.x, desiredRotation, transform.eulerAngles.z);
+            transform.rotation = Quaternion.Lerp(transform.rotation, desiredRotationQuat, Time.deltaTime * rotationDamping);
+            if (randomSeed <= pausePercentage) {
+                transform.position += transform.forward * Time.deltaTime * moveSpeed;
+            }
         }
     }
 
     private void OnCollisionEnter(Collision collision) {
-        if (collision.gameObject.CompareTag("wall") || collision.gameObject.CompareTag("piller") || collision.gameObject.CompareTag("lightbowl")) {
+        //collisions with walls causes the object to spin 180 to prevent AI from ramming wall (only when not chasing)
+        if ((collision.gameObject.CompareTag("wall") || collision.gameObject.CompareTag("piller") || collision.gameObject.CompareTag("lightbowl")) && isAlerted == false) {
             ChangeDirection(true);
         }
     }
@@ -39,7 +53,7 @@ public class EnemyController : MonoBehaviour {
     private void ChangeDirection(bool isWall) {
         //if it hits wall only allow for rotations between 90-270 degrees
         if (isWall) {
-            desiredRotation = desiredRotation + 180f;
+            desiredRotation = transform.eulerAngles.y + 180f;
         } else {
             desiredRotation = Random.Range(0f, 360f);
         }
