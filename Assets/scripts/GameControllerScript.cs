9ed731915367a9e10e57ab7d42ff5aa40c78cb20
Weihan Li <weihanli101@gmail.com>
2018-09-04 18:06:17 -0400

    added in particle effect when picking up light orbs + added in sound effect for picking up light orb + added in main menu + add in game over menu

--- Assets/scripts/GameControllerScript.cs
+++ Assets/scripts/GameControllerScript.cs
@@ -1,17 +1,79 @@
-﻿using System.Collections;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameControllerScript : MonoBehaviour {
+    //game balancing values
+    public float scoreAddingFreqency;
+    public GameObject gameOverMenu;
+    public GameObject player;
+ 
+    //UI
+    public Text scoreText;
+    public Slider healthBar;
+    public GameObject joystick;
+    public Text highScoreText;
+    public Text finalScoreText;
+
+    private int score;
+    private int highScore;
 	// Use this for initialization
 	void Start () {
+        score = 0;
+        InvokeRepeating("AddScore", scoreAddingFreqency, scoreAddingFreqency);
 	}
 
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        //game over
+        if(player.GetComponent<CharacterController>().health <= 0) {
+            CancelInvoke();
+            endGame();
+        } else {
+            UpdateUI();
+        }
+
+    }
 
     private void endGame() {
-        //TODO: End game stuff
+        //show gameover menu
+        gameOverMenu.SetActive(true);
+
+        //hide other ui items
+        scoreText.gameObject.SetActive(false);
+        healthBar.gameObject.SetActive(false);
+        joystick.SetActive(false);
+
+        //set final score text
+        if (PlayerPrefs.HasKey("highestScore")) {
+            highScore = PlayerPrefs.GetInt("highestScore");
+
+            //set new highscore if greater
+            if(score > highScore) {
+                highScore = score;
+                PlayerPrefs.SetInt("highestScore",highScore);
+            }
+        }
+        else {
+            highScore = score;
+        }
+        finalScoreText.text = "Score: " + score.ToString();
+        highScoreText.text = "Best: " + highScore.ToString();
+    }
+
+    public void onRestart() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+    
+    public void onExit() {
+        SceneManager.LoadScene(0);
+    }
+
+    private void AddScore() {
+        score += 1;
+    }
+    
+    private void UpdateUI() {
+        scoreText.text = "Score: " + score.ToString();
     }
 }
--- Assets/scripts/characterController.cs
+++ Assets/scripts/characterController.cs
@@ -7,13 +7,18 @@ public class CharacterController : MonoBehaviour {
     public float playerMinLightIntenstiy;
     public float lightDecayRate;
     public int health;
+    public float AttackRadius;
+    public float AttackPower;
+    public float AttackDuration;
 
     //Audio
     public AudioClip hitAudioClip;
+    public AudioClip orbAudioClip;
     private AudioSource audioSource;
 
     //Particles
     public GameObject bloodParticles;
+    public GameObject explosionParticles;
 
     private void Start() {
         audioSource = GetComponent<AudioSource>();
@@ -37,14 +42,19 @@ public class CharacterController : MonoBehaviour {
     private void OnCollisionEnter(Collision collision) {
         //reset lightIntensity
         if (collision.gameObject.CompareTag("lightorb")) {
+            explosionParticles.GetComponent<ParticleSystem>().Play();
             playerMaxLightIntensity = 50;
+            audioSource.PlayOneShot(orbAudioClip);
             Destroy(collision.gameObject);
+            explode();
         }
         //hit enemy: lose health + play hit audio + play blood particles
         if(collision.gameObject.CompareTag("enemy")) {
             bloodParticles.GetComponent<ParticleSystem>().Play();
             audioSource.PlayOneShot(hitAudioClip);
-            health -= 10;
+            if(health > 0) {
+                health -= 10;
+            }
         }
     }
 
@@ -52,6 +62,19 @@ public class CharacterController : MonoBehaviour {
         //only detuct till 0
         if(playerMaxLightIntensity >= playerMinLightIntenstiy) {
             playerMaxLightIntensity -= lightDecayRate * Time.deltaTime;
+        }
+    }
+
+    //adds explosive force to nearby enemies
+    private void explode() {
+        Vector3 explosionPos = transform.position;
+        Collider[] colliders = Physics.OverlapSphere(explosionPos, AttackRadius);
+        foreach(Collider hit in colliders) {
+            Rigidbody enemyRb = hit.GetComponent<Rigidbody>();
+
+            if (enemyRb != null && enemyRb.CompareTag("enemy"))  {
+                enemyRb.AddExplosionForce(AttackPower, explosionPos, AttackRadius, AttackDuration);
+            }
         }
     }
 
