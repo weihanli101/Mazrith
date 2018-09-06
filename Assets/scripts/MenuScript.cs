8fcecf328cd8ba2700106a1a7231a3110fa1e24b
Weihan Li <weihanli101@gmail.com>
2018-09-05 22:13:57 -0400

    create help scene + credits scene + added in animated backgrounds for both

--- Assets/scripts/MenuScript.cs
+++ Assets/scripts/MenuScript.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MenuScript : MonoBehaviour {
@@ -17,6 +15,18 @@ public class MenuScript : MonoBehaviour {
 
     public void PlayGame() {
         SceneManager.LoadScene(1);
+    }
+
+    public void Help() {
+        SceneManager.LoadScene(2);
+    }
+
+    public void ReturnToMain() {
+        SceneManager.LoadScene(0);
+    }
+
+    public void Credits() {
+        SceneManager.LoadScene(3);
     }
     
 }
